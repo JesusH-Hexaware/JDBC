24486107530d5686b81b640f3c6c9f5a0c16cbf2
Jesús Hernández Aguilar <JesusH@hexaware.com>
2022-01-31 10:01:26 -0600

    Create database

--- src/main/java/com/hexaware/JDBC.java
+++ src/main/java/com/hexaware/JDBC.java
@@ -1,10 +1,33 @@
 package com.hexaware;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class JDBC {
 
     public static void main(String[] args) {
-
+        createDatabase();
     }
 
+    public static void createDatabase(){
+        Connection conexion = null;
+        Statement stmt = null;
+        String newDatabaseName = "testDatabase";
 
+        try{
+            conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/", "root", "Spellbook.10");
+            stmt = conexion.createStatement();
+            stmt.executeUpdate("CREATE DATABASE " + newDatabaseName);
+            System.out.println(newDatabaseName + " Database created!");
+        } catch (SQLException sqlException) {
+            if (sqlException.getErrorCode() == 1007){
+                //Database already exists error
+                System.out.println(sqlException.getMessage());
+            }else{
+            sqlException.printStackTrace();
+            }
+        }
+    }
 }
